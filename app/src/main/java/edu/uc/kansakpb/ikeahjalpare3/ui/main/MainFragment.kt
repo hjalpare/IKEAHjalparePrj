4421648335ffca01da3870aec5de13ac6116195e
Bob Freid <freidrd@mail.uc.edu>
2020-07-02 18:23:55 -0400

    Removed import ViewModelProviders from MainFragment.kt See: https://stackoverflow.com/questions/53903762/viewmodelproviders-is-deprecated-in-1-1-0

--- app/src/main/java/edu/uc/kansakpb/ikeahjalpare3/ui/main/MainFragment.kt
+++ app/src/main/java/edu/uc/kansakpb/ikeahjalpare3/ui/main/MainFragment.kt
@@ -1,11 +1,10 @@
 package edu.uc.kansakpb.ikeahjalpare3.ui.main
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import edu.uc.kansakpb.ikeahjalpare3.R
 
