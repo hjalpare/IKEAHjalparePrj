de9fff8bbeeda18a1fa2ccbb1faaa399995ed783
Bob Freid <freidrd@mail.uc.edu>
2020-07-22 10:01:23 -0400

    Removed inline comments in the FurnitureService.kt file.

--- app/src/main/java/edu/uc/kansakpb/ikeahjalpare3/service/FurnitureService.kt
+++ app/src/main/java/edu/uc/kansakpb/ikeahjalpare3/service/FurnitureService.kt
@@ -11,23 +11,19 @@ import retrofit2.Response
 class FurnitureService {
 
     internal fun fetchFurnitures(furnitureName: String) : MutableLiveData<ArrayList<Furniture>> {
-        //MVD is defined
         var _furnitures = MutableLiveData<ArrayList<Furniture>>()
         val service = RetrofitClientInstance.retrofitInstance?.create(IFurnitureDAO::class.java)
         val call = service?.getAllFurnitures()
         call?.enqueue(object: Callback<ArrayList<Furniture>> {
-            //Call onResponse to determine if the response indicates success.
             override fun onResponse(
                 call: Call<ArrayList<Furniture>>,
                 response: Response<ArrayList<Furniture>>
             ) {
                 _furnitures.value = response.body()
             }
-            //Call onFailure to determine if the response indicates success.
             override fun onFailure(call: Call<ArrayList<Furniture>>, t: Throwable) {
                 val j = 1 + 1
                 val i = 1 + 1
-                // throw Exception("Unable to parse json file")
             }
         })
         return _furnitures
