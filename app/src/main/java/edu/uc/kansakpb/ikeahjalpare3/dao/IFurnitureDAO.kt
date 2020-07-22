044acc482e190e629bc9c7d3acd072ba91e25636
AirPro <AirPro@2016>
2020-07-22 10:45:16 -0400

    changed fike IFurnitureDAO.kt added option to @GET

--- app/src/main/java/edu/uc/kansakpb/ikeahjalpare3/dao/IFurnitureDAO.kt
+++ app/src/main/java/edu/uc/kansakpb/ikeahjalpare3/dao/IFurnitureDAO.kt
@@ -3,8 +3,13 @@ package edu.uc.kansakpb.ikeahjalpare3.dao
 import edu.uc.kansakpb.ikeahjalpare3.dto.Furniture
 import retrofit2.http.GET
 import retrofit2.Call
+import retrofit2.http.Query
 
 interface IFurnitureDAO {
+
     @GET("/ikeafurniture.json")
     fun getAllFurnitures(): Call<ArrayList<Furniture>>
+
+    @GET("/ikeafurniture.json")
+    fun getFurniture(@Query("Combined_Name")funritureName: String): Call <ArrayList<Furniture>>
 }
