74ceeb6a62b2577a6615ffcb1516b2541c3780f8
oblackta <oblackta@mail.uc.edu>
2020-07-27 17:03:34 -0400

    adding search button

--- app/src/main/java/edu/uc/kansakpb/ikeahjalpare3/MainActivity.kt
+++ app/src/main/java/edu/uc/kansakpb/ikeahjalpare3/MainActivity.kt
@@ -1,7 +1,9 @@
 package edu.uc.kansakpb.ikeahjalpare3
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import edu.uc.kansakpb.ikeahjalpare3.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -14,5 +16,13 @@ class MainActivity : AppCompatActivity() {
                     .replace(R.id.container, MainFragment.newInstance())
                     .commitNow()
         }
+        //searchButton()
     }
+//    private fun searchButton() {
+//        val btnSearch = findViewById<ImageButton>(R.id.btnSearch)
+//        btnSearch.setOnClickListener {
+//            val searchIntent = Intent(this, FurnitureActivity::class.java)
+//            startActivity(searchIntent)
+//        }
+//    }
 }
