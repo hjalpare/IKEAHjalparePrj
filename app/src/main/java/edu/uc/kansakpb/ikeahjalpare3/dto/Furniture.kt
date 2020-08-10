a2f293309c925aed562a1de807ff84b08a557f5a
Bob Freid <freidrd@mail.uc.edu>
2020-08-09 20:42:05 -0400

    Gave Furnture Class properties default values

--- app/src/main/java/edu/uc/kansakpb/ikeahjalpare3/dto/Furniture.kt
+++ app/src/main/java/edu/uc/kansakpb/ikeahjalpare3/dto/Furniture.kt
@@ -3,11 +3,11 @@ package edu.uc.kansakpb.ikeahjalpare3.dto
 import com.google.gson.annotations.SerializedName
 
 data class Furniture(
-    @SerializedName("id")var id:Int,
-    @SerializedName("article_number")var articleNumber: String,
-    @SerializedName("furniture_name") var furnitureName: String,
-    @SerializedName("instructions_manuel")var instructionsManuel: String,
-    @SerializedName("video_link")var videoLink: String
+    @SerializedName("id")var id:Int = 0,
+    @SerializedName("article_number")var articleNumber: String = "",
+    @SerializedName("furniture_name") var furnitureName: String = "",
+    @SerializedName("instructions_manuel")var instructionsManuel: String = "",
+    @SerializedName("video_link")var videoLink: String = ""
 ) {
     override fun toString(): String {
         return "$furnitureName $id $articleNumber $instructionsManuel $videoLink"
